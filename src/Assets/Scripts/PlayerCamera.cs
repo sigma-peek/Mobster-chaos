c378b2027df552e327e5f7e20b8b90cd6597ca42
KadlecJir <145103778+KadlecJir@users.noreply.github.com>
2024-02-16 12:22:53 +0100

    added camera movement

---
+++ src/Assets/Scripts/PlayerCamera.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerCamera : MonoBehaviour
+{
+    private GameObject playerParent;
+    private Vector2 inputVector, inputVectorSmooth, cameraCurrent;
+    private float sensitivty = 0.5f, smoothing = 0.5f;
+
+    void Awake()
+    {
+        playerParent = transform.parent.gameObject;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+    }
+
+    private void OnCameraLook(InputValue inputValue) {
+        inputVector = inputValue.Get<Vector2>();
+        inputVector = Vector2.Scale(inputVector, new Vector2(sensitivty * smoothing, sensitivty * smoothing));
+
+        inputVectorSmooth.x = Mathf.Lerp(inputVectorSmooth.x, inputVector.x, 1f/smoothing);
+        inputVectorSmooth.y = Mathf.Lerp(inputVectorSmooth.y, inputVector.y, 1f/smoothing);
+
+        cameraCurrent += inputVectorSmooth;
+
+        transform.localRotation = Quaternion.AngleAxis(-cameraCurrent.y, Vector3.right);
+        playerParent.transform.localRotation = Quaternion.AngleAxis(cameraCurrent.x, playerParent.transform.up);
+    }
+}
--- src/Assets/Scripts/PlayerMovement.cs
+++ src/Assets/Scripts/PlayerMovement.cs
@@ -7,25 +7,34 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] CharacterController controller;
-
+    [SerializeField] GameObject playerCamera;
     private Vector2 inputVector;
-    private Vector3 moveVector;
-    private float playerSpeed = 10;
+    private Vector3 moveVector, forwardVector, rightVector, gravityVector;
+    private float playerSpeed = 6;
 
     // Start is called before the first frame update
-    void Start()
-    {        
+    void Awake()
+    {       
+        gravityVector = Vector3.zero; 
     }
 
     // Update is called once per frame
     void Update()
     {
         controller.Move(playerSpeed * moveVector * Time.deltaTime);
+
+        gravityVector.y += -20 * Time.deltaTime;
+        controller.Move(gravityVector * Time.deltaTime);
     }
 
     private void OnMovement(InputValue inputValue) {
         inputVector = inputValue.Get<Vector2>();
         moveVector.x = inputVector.x;
         moveVector.z = inputVector.y;
+
+        forwardVector = playerCamera.transform.forward * inputVector.y;
+        rightVector = playerCamera.transform.right * inputVector.x;
+
+        moveVector = forwardVector + rightVector;
     }
 }
